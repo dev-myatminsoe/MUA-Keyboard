4c2f07bb6f7206637224165630cc70fea381f2fe
Myat Min Soe <myatminsoe@Myats-MacBook-Air.local>
2015-07-21 02:02:57 +0630

    Change to gradle based Project

---
+++ app/build/generated/source/buildConfig/debug/com/sanlin/mkeyboard/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sanlin.mkeyboard;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.sanlin.mkeyboard";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "";
+}
